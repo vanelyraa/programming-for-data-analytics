e73d8e748250148e0a0ab6e8459e9940afa3cd6a
vanelyra <vanelyra@hotmail.com>
2025-10-11 18:17:25 +0100

    Assignment 3

---
+++ assignment02-bankholdiays.py
@@ -0,0 +1,10 @@
+
+# Assignment 2 - Bank holidays
+# Author: Vanessa Lyra
+
+'''Northern bank holdays'
+Write a program called assignment02-bankholdiays.py
+The program should print out the dates of the bank holidays that happen in northern Ireland.
+Last few marks (ie this is more tricky)
+Modify the program to print the bank holidays that are unique to northern Ireland (i.e. do not happen elsewhere in the UK) you can choose if you want to use the name 
+or the date of the holiday to decide if it is unique.'''
